dc89410e27b8cf2e340ede9399c0dd18a1532e84
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2019-06-26 16:52:04 +0200

    board-kata

---
+++ board-kata-master/parser/parser.go
@@ -0,0 +1,12 @@
+package parser
+
+const (
+	baseURL    = "https://fogo-parser.dev/"
+	hashTagURL = "https://fogo-parser.dev/hash/"
+)
+
+// Parse fomating the text into a valid output text
+func Parse(msg string) (output string) {
+	// code here
+	return output
+}
